a1a8325c692874723f2f43af63dd240eec6442c3
jaloliddinr <86194476+jaloliddinr@users.noreply.github.com>
2022-01-24 02:25:25 -0500

    Add files via upload

---
+++ website.js
@@ -0,0 +1,10 @@
+document.querySelector(".order").onclick = function(){
+    console.log("work")
+    alert("Sorry but I didnt make this portion of the site yet :(")
+}
+document.querySelector(".orderer").onclick = function(){
+    alert("Sorry but I didnt make this portion of the site yet :(")
+}
+document.querySelector(".order-online").onclick = function(){
+    alert("Sorry but I didnt make this portion of the site yet :(")
+}
